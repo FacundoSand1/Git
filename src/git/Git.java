d6e7c9adfdd8f10d5c32a5230693a90b6e5b36cd
Facundo Sandoval <facundossandovalcontacto@gmail.com>
2023-03-05 05:03:39 -0300

    tercer commit

--- src/git/Git.java
+++ src/git/Git.java
@@ -17,6 +17,9 @@ public class Git {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo 1");
+        System.out.println("Hola mundo 2");
+        
+        
     }
     
 }
